18e2bb63247877cb3d76e15c736a063424680641
VladOlevskiy <99341001+VladOlevskiy@users.noreply.github.com>
2022-12-09 18:58:31 +0200

    base

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,13 @@
+import { NavLink } from './Navigation-styled';
+import { useAuth } from '../../hooks/useAuth';
+
+export const Navigation = () => {
+  const { isLoggedIn } = useAuth();
+
+  return (
+    <nav>
+      <NavLink to="/">Home</NavLink>
+      {isLoggedIn && <NavLink to="/contacts">Contacts</NavLink>}
+    </nav>
+  );
+};
---
+++ src/redux/contacts/operations.js
@@ -0,0 +1,41 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+// axios.defaults.baseURL = 'https://6380e12e8efcfcedac11ec64.mockapi.io';
+// axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
+
+export const fetchContacts = createAsyncThunk(
+  'contacts/fetchAll',
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get('/contacts');
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const addContact = createAsyncThunk(
+  'contacts/addContact',
+  async (values, thunkAPI) => {
+    try {
+      const response = await axios.post('/contacts', values);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const deleteContact = createAsyncThunk(
+  'contacts/deleteContact',
+  async (contactId, thunkAPI) => {
+    try {
+      const response = await axios.delete(`/contacts/${contactId}`);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
